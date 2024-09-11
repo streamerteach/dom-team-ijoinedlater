3c642fd0d5935e5b2b445b04fbd4f2cbea96ed92
Axel-blomq <149474745+Axel-blomq@users.noreply.github.com>
2024-09-11 10:08:35 +0300

    changes

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 window.onload = function() {
     // Lyssna efter händelser
+    var Hoverable = document.getElementsByClassName("team-member");
+    for (var i = 0; i < Hoverable.length; i++){
+        Hoverable[i].id = 'Hoverable' + i;
+        Hoverable[i].addEventListener("mouseover", effectOn(Hoverable[i].id));
+        alert("pause");
+        Hoverable[i].addEventListener("mouseout", effectOff(Hoverable[i].id));
+    }   
 }
 var editTitle = function(e) {
     // Ändra "team-title"
@@ -7,8 +14,12 @@ var editTitle = function(e) {
 
 function effectOn(id){
     // Rita en ram runt personen, kanske lite drop shadow?
+    document.getElementById(id).style.filter = "drop-shadow(30px 10px 4px #4444dd)";
+    document.getElementById(id).style.border = "5px,solid,black";
 }
 
 function effectOff(id){
     // Stäng av effekten när musen inte längre är ovanför personen
+    document.getElementById(id).style.filter = "none";
+    document.getElementById(id).style.border = "none";
 }
